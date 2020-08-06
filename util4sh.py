5e7b2c84e2471d7d18dca27cbc68d344a3bb90e5
shewinder <7467498+luo_zhen_xu@user.noreply.gitee.com>
2020-08-06 23:16:42 +0800

    修改了Res类，适配go-cqhttp

--- util4sh.py
+++ util4sh.py
@@ -30,30 +30,43 @@ def get_random_file(path) -> str:
     rfile = random.choice(files)
     return rfile
 
+import hashlib
+def get_str_md5(text: str) -> str:
+    m = hashlib.md5()
+    m.update(text.encode('utf-8'))
+    md5_str = m.hexdigest()
+    return md5_str
+
 class Res:
-    res_dir = path.join(path.dirname(__file__), 'modules', 'shebot', 'res')
+    res_dir = path.join(path.dirname(__file__), 'modules','shebot', 'res')
     image_dir = path.join(res_dir, 'image')
     record_dir = path.join(res_dir, 'record')
     @classmethod
     def image(cls, pic_path: str) -> 'MessageSegment':
-        return MessageSegment.image(path.join(cls.image_dir, pic_path))
+        return MessageSegment.image(f'file:///{path.join(cls.image_dir, pic_path)}')
 
     @classmethod
     def record(cls, rec_path) -> 'MessageSegment':
-        return MessageSegment.record(path.join(cls.record_dir, rec_path))
+        return MessageSegment.record(f'file:///{path.join(cls.record_dir, rec_path)}')
 
     @classmethod
-    async def save_image(cls, event: Event) -> None:
+    async def save_image(cls, event: Event, folder=None) -> None:
+        if not folder:
+            image_path = cls.image_dir
+        else:
+            image_path = path.join(cls.image_dir, folder)
+        if not path.isdir(image_path):
+            os.mkdir(image_path)
         for i, m in enumerate(event.message):
             match = re.match('\[CQ:image.+?\]', str(m))
             if match:
                 try:
                     url = re.findall(r'http.*?term=\d', str(m))[0]
                     save_name = re.findall(r'(?<=-)[^-]*?(?=/)',url)[0]
-                    image_path = await download_async(url, cls.image_dir, save_name)
-                    event.message[i] = MessageSegment.image(image_path)
+                    image = await download_async(url, image_path, save_name)
+                    event.message[i] = MessageSegment.image(f'file:///{image}')
                 except Exception as ex:
-                    logger.warning(f'保存图片时发生错误{ex}')
+                    print_exc()
         event.raw_message = str(event.message)
 
     @classmethod
@@ -63,7 +76,15 @@ class Res:
         else:
             image_path = path.join(cls.image_dir, folder)
         image_name = get_random_file(image_path)
-        return MessageSegment.image(path.join(image_path, image_name))
+        return MessageSegment.image(f'file:///{path.join(image_path, image_name)}')
+
+    @classmethod
+    async def image_from_url(cls, url: str, cache=True) -> 'MessageSegment':
+        fname = get_str_md5(url)
+        image = path.join(cls.image_dir, f'{fname}.jpg')
+        if not path.exists(image) or not cache:
+            image = await download_async(url, cls.image_dir, fname, 'jpg')
+        return MessageSegment.image(f'file://{image}') 
 
 from nonebot import scheduler
 import datetime
